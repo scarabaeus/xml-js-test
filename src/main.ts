f9891908574018544bc66c910e6a7a0b05a61893
Steve Gomez <StevenAGomez@Live.com>
2024-01-16 10:47:00 -0800

    Adds xml-js

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import express from 'express';
+import XmlJs from 'xml-js';
+
 const app = express();
 
+// const xml = `<?xml version="1.0" encoding="utf-8"?>
+//   <note importance="high" logged="true">
+//       <title>Happy</title>
+//       <todo>Work</todo>
+//       <todo>Play</todo>
+//   </note>`;
+
+const xml = `<SOAP-ENV:Body xmlns:m = "http://www.xyz.org/quotation">
+<m:GetQuotationResponse>
+   <m:Quotation>Here is the quotation</m:Quotation>
+</m:GetQuotationResponse>
+</SOAP-ENV:Body>`;
+
 app.get('/', function (req, res) {
-  res.send('ts-node-18-express');
+  const compact = XmlJs.xml2json(xml, { compact: true, spaces: 4 });
+  const nonCompact = XmlJs.xml2json(xml, { compact: false, spaces: 4 });
+
+  res.json({
+    compact: JSON.parse(compact),
+    nonCompact: JSON.parse(nonCompact),
+  });
 });
 
 app.listen(3000);
